f4671045ab6a2a142d18a899af323120b2ad13e8
Sinan Sakaoğlu <sinansakaoglu@outlook.com>
2020-02-19 20:19:19 +0100

    chore: added eslint and prettier

--- src/index.js
+++ src/index.js
@@ -10,25 +10,20 @@ const io = SocketIo(server);
 
 app.use(Cors());
 app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({
+app.use(
+  BodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
 app.get("/", async (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 
 io.on("connection", async socket => {
-    console.log("socket connected");
+  console.log("socket connected");
 });
 
 server.listen(7878, () => {
-    console.log("Server listening on port 7878");
+  console.log("Server listening on port 7878");
 });
-
-
-
-
-
-
-
